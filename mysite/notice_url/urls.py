de2e22c68ad6e9ae37b5d89dc627829d60890faa
IC-H <insectchung@naver.com>
2018-09-02 02:30:54 +0900

    【notice_url】adding search page ajax file

--- mysite/notice_url/urls.py
+++ mysite/notice_url/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+from . import views, ajax
 
 urlpatterns = [
     path('search/', views.search, name='search'),
+    path('search/ajax/', ajax.search, name='search_ajax'),
 ]
