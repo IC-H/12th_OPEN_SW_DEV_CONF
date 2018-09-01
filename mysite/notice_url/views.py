6029c99ff0b0725b78921293cc6b9db886b38a1e
IC-H <insectchung@naver.com>
2018-09-02 02:30:23 +0900

    【notice_url】apply static stylesheet and make view

--- mysite/notice_url/views.py
+++ mysite/notice_url/views.py
@@ -2,4 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def search(request):
-    return HttpResponse("Hello, world. You're at the notice_url search.")
+    data = {
+        'domain_list' : [
+            {
+                'domain'    : 'domain1',
+                'url_list'  : ['url1', 'url2']
+            },
+            {
+                'domain'    : 'domain2',
+                'url_list'  : ['url1']
+            }
+        ]
+    }
+    return render(request, 'search/index.html', data);
