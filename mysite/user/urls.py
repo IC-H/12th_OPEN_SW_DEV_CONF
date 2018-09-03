37eddec238d1ce55d578ddb6da2e3052ebd97327
miintto <minjp93@gmail.com>
2018-09-04 02:24:14 +0900

    struct login form

--- mysite/user/urls.py
+++ mysite/user/urls.py
@@ -3,7 +3,7 @@ from . import views
 from .url import views as url_views
 
 urlpatterns = [
-    path('sign_in/', views.sign_in, name = 'sign_in'),
-    path('sign_up/', views.sign_up, name = 'sign_up'),
+    path('sign_in/', views.sign_in.as_view(), name='sign_in'),
+    path('sign_up/', views.sign_up.as_view(), name = 'sign_up'),
     path('url/register/', url_views.registerView.as_view(), name = 'url_register'),
 ]
