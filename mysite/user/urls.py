03ffbd02064cff54d2f7ef5b3af8dfe48223b124
IC-H <insectchung@naver.com>
2018-09-10 22:28:54 +0900

    【URL 등록 화면】URL 등록 화면 뷰 작성

--- mysite/user/urls.py
+++ mysite/user/urls.py
@@ -5,5 +5,5 @@ from .url import views as url_views
 urlpatterns = [
     path('sign_in/', views.sign_in, name = 'sign_in'),
     path('sign_up/', views.sign_up, name = 'sign_up'),
-    path('url/register/', url_views.register, name = 'url_register'),
+    path('url/register/', url_views.registerView.as_view(), name = 'url_register'),
 ]
