5b159ebd131f36739bc4f95c538c84f190efd088
IC-H <insectchung@naver.com>
2018-09-10 22:35:38 +0900

    【URL 검색화면】URL 검색 화면 View 클래스 적용

--- mysite/notice_url/urls.py
+++ mysite/notice_url/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views, ajax
 
 urlpatterns = [
-    path('search/', views.search, name='search'),
+    path('search/', views.searchView.as_view(), name='search'),
     path('search/ajax/', ajax.search, name='search_ajax'),
 ]
