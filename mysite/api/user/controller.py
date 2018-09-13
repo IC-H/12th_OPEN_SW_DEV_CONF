77b25a5f7d1e604c8b80af4c37a3af2dcc9b22d9
kproject_t.hara-bp <t.hara-bp@kproject.co.jp>
2018-09-14 04:18:06 +0900

    【URL 등록 API】컨트롤러 작성

--- mysite/api/user/controller.py
+++ mysite/api/user/controller.py
@@ -1,7 +1,33 @@
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
-from common.models import User
+from django.contrib import messages
+from django.db import DatabaseError, IntegrityError, transaction
+from common.models import User, UserUrl, DomainUrl
 
+@transaction.atomic
 def url_register(request):
+    # TODO get user from session
+    try:
+        user = User.objects.filter(pk__exact=1).get()
+    except User.DoesNotExist:
+        messages.add_message(request, messages.ERROR, 'Please Log In')
+        return redirect(reverse_lazy('sign_in'))
     
-    return redirect(reverse_lazy('search'))
+    added_url_id_list   = request.POST.getlist('added_url_id_list[]')
+    deleted_url_id_list = request.POST.getlist('deleted_url_id_list[]')
+    try:
+        with transaction.atomic():
+            if len(added_url_id_list) > 0:
+                for url_obj in DomainUrl.objects.filter(pk__in=added_url_id_list):
+                    user_url_obj = UserUrl(
+                        user=user,
+                        url=url_obj
+                    ).save()
+            if len(deleted_url_id_list) > 0:
+                UserUrl.objects.filter(user__id__exact=user.id).filter(url__id__in=deleted_url_id_list).delete()
+    except (Exception) as e:
+        messages.add_message(request, messages.ERROR, e)
+        return redirect(reverse_lazy('search'))
+    
+    messages.add_message(request, messages.SUCCESS, 'Success for saving')
+    return redirect(reverse_lazy('url_list'))
