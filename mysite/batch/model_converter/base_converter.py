1ff57c9ed7d60f8c7d40dfc7bd4312e4c92dbe22
IC-H <insectchung@naver.com>
2018-09-24 06:30:50 +0900

    #46 【백 엔드】【쉘】 vectorize ・domain_mst, domain_url에서 각각 레코드 얻어서 내부 프로퍼티에 저장

--- mysite/batch/model_converter/base_converter.py
+++ mysite/batch/model_converter/base_converter.py
@@ -1,7 +1,9 @@
 from django.core.exceptions import FieldError, ValidationError
 from django.db.models import Model, QuerySet
 from django.db import transaction, DatabaseError, IntegrityError
+from common.models import DomainMst, DomainUrl
 import requests
+import re
 
 class MetaConverter(type):
     def __new__(cls, name, bases, attrs, **kwargs):
@@ -33,6 +35,8 @@ class BaseModelConverter(metaclass=MetaConverter):
     
     def initialize(self):
         self.html = None
+        self._domain_url_model = None
+        self._domain_mst_model = None
         self._vector_model_set = []
     
     def validate_response(self, response):
@@ -43,8 +47,51 @@ class BaseModelConverter(metaclass=MetaConverter):
         if not isinstance(model, self.vector_model):
             raise TypeError('model has to be instance of vector_model')
     
+    @transaction.atomic
+    def identify_url(self, response):
+        '''
+        Identiry response url
+        If url's domain and url are already registed their table, read it.
+        If not registed, regist them and read it.
+        '''
+        domain = re.search(r"((?<=(^http:\/\/))|(?<=^https:\/\/))[^\/]*", response.url).group()
+        url = re.search(r"((?<=(^http:\/\/))|(?<=^https:\/\/)).*", response.url).group()
+        try:
+            domain_model = DomainMst.objects.filter(domain__exact=domain).get()
+        except DomainMst.DoesNotExist:
+            try:
+                with transaction.atomic():
+                    domain_model = DomainMst(domain=domain)
+                    domain_model.save()
+            except (DatabaseError, IntegrityError):
+                # For parallel thread
+                # If domain is registerd with another process search one more time
+                domain_model = DomainMst.objects.filter(domain__exact=domain)
+        try:
+            url_model = DomainUrl.objects.filter(url__exact=url).get()
+        except DomainUrl.DoesNotExist:
+            try:
+                with transaction.atomic():
+                    url_model = DomainUrl(domain=domain_model, url=url)
+                    url_model.save()
+            except (DatabaseError, IntegrityError):
+                # For parallel thread
+                # If domain is registerd with another process search one more time
+                url_model = DomainUrl.objects.filter(url__exact=url)
+        self._domain_mst_model = domain_model
+        self._domain_url_model = url_model
+    
+    @property
+    def domain_url_model(self):
+        return self._domain_url_model
+    
+    @property
+    def domain_mst_model(self):
+        return self._domain_mst_model
+    
     def run(self, response):
         self.validate_response(response)
+        self.identify_url(response)
         self.convert(response)
     
     def append_vector_model_set(self, model):
