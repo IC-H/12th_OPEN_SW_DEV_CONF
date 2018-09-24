24fdb5cea61bac37284ab0077b7967efc3f0c5f8
IC-H <insectchung@naver.com>
2018-09-25 00:24:52 +0900

    #46 【백 엔드】【쉘】 vectorize ・html_model converter 작성

--- mysite/batch/model_converter/__init__.py
+++ mysite/batch/model_converter/__init__.py
@@ -1,5 +1,6 @@
 from .base_converter import BaseModelConverter
+from .html_model_converter import HtmlVectorModelConverter
 
 __all__ = [
-    'BaseModelConverter'
+    'BaseModelConverter', 'HtmlVectorModelConverter'
 ]
