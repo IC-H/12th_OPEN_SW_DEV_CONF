09d6781d56983124314f6c5f53d7087753d7a2e9
miintto <minjp93@gmail.com>
2018-09-25 15:42:40 +0900

    【SHELL 이메일알람】디렉토리 수정 및 클래스 변환

---
+++ mysite/batch/mail/send_mail.py
@@ -0,0 +1,41 @@
+from django.conf import settings
+from common.models import User, UserUrl, DomainUrl
+from django.core.mail import send_mail
+ 
+class YummyMailer():
+
+    def __init__(self):
+        self.set_url_list()
+
+    def find_changed_url(self):
+    	domain_url = []
+    	for changed_url in DomainUrl.objects.filter(has_change__exact = 1):
+    	    domain_url.append(changed_url.id)
+    	return domain_url
+
+    def send_mail_by_url(self, url_id):
+        user = UserUrl.objects.filter(url__exact = url_id)
+        user_email = []
+        for user_url in user:
+            user_email.append(User.objects.filter(pk__exact = user_url.user_id).get().email)
+
+        if len(user_email) > 0:
+            send_mail(
+                subject = 'Yummy Spam : 새로운 공지가 업데이트 되었습니다.',
+                message = DomainUrl.objects.filter(pk__exact = url_id).get().url+' 에서 새로운 공지가 업데이트 되었습니다.',
+                from_email = settings.EMAIL_HOST_USER,
+                recipient_list = [user_email],
+                fail_silently = False,
+            )
+
+    def update_has_change(self, url_id):
+        DomainUrl.objects.filter(pk__exact = url_id).update(has_change = False)
+
+    def send(self):
+        url = find_changed_url()
+        for _id in url:
+            send_mail_by_url(_id)
+            update_has_change(_id)
+
+    def set_url_list(self):
+        self.url_list = self.find_changed_url()
--- mysite/user/backend/backends.py
+++
@@ -1,8 +0,0 @@
-from user.backend.mail.mail import find_changed_url, send_mail_by_url
-
-
-if ## When?:
-    url = find_changed_url()
-
-    for _id in url:
-	    send_mail_by_url(_id)
