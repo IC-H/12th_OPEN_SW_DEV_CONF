ceb8df797dd2d24ba42107cb00a4152b838ac51a
IC-H <insectchung@naver.com>
2018-10-01 02:16:05 +0900

    【COMMON MODELS】【학습】 ・크롤러

--- mysite/batch/crawler/__init__.py
+++ mysite/batch/crawler/__init__.py
@@ -2,8 +2,7 @@ from .base_crawler import BaseCrawler
 from .notice_crawler import NoticeCrawler
 from .notice_crawler_lite import NoticeCrawlerLite
 from .notice_url_crawler import NoticeUrlCrawler
-from .notice_url_crawler_lite import NoticeUrlCrawlerLite 
 
 __all__ = [
-    'BaseCrawler', 'NoticeCrawler', 'NoticeCrawlerLite', 'NoticeUrlCrawler', 'NoticeUrlCrawlerLite'
+    'BaseCrawler', 'NoticeCrawler', 'NoticeCrawlerLite', 'NoticeUrlCrawler'
 ]
